ac9a96059e6696f3f9a768dbd6fe78c7e967f2ab
aspROMO <aspromo.cvt@eac.edu.ph>
2024-02-20 18:17:26 +0800

    Double jump and character dimension change

--- scenes/entities/ZedCharacterBody2D.gd
+++ scenes/entities/ZedCharacterBody2D.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-
-@export var speed : float = 300.0
-@export var jump_force : float = -350.0
+@export var speed : float = 275.0
+@export var jump_force : float = -300.0
 @export var jump_time : float = 0.25
 @export var coyote_time : float = 0.075
 @export var gravity_multiplier : float = 3.0
+@export var double_jump_force : float = -350.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var can_double_jump : bool = false
+var double_jump_timer : float = 0.0
+var num_jumps : int = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,8 +18,16 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_force
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_force
+			num_jumps = 1
+			can_double_jump = true
+		elif can_double_jump and num_jumps < 2:
+			velocity.y = double_jump_force
+			num_jumps += 1
+			can_double_jump = false
+			double_jump_timer = 0.0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,3 +38,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+func _process(delta):
+	if not is_on_floor():
+		double_jump_timer += delta
+		if double_jump_timer > coyote_time:
+			can_double_jump = true
