fd094ca0673a40e21c76d69c0d1c5455936b8b94
rsnavarro <139185661+rochellenavarro@users.noreply.github.com>
2024-02-22 17:16:08 +0800

    Added Zed's Animation and danger zone
    Danger zone for water in level 1

--- assets/Scripts/classes/level.gd
+++ assets/Scripts/classes/level.gd
@@ -2,6 +2,7 @@ class_name Level
 extends Node
 
 @export var level_id : int
+@export var level_start_pos : Node2D
 
 var level_data : LevelData
 
--- scenes/entities/ZedCharacterBody2D.gd
+++ scenes/entities/ZedCharacterBody2D.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends CharacterBody2D
 
 @export var speed : float = 275.0
@@ -11,8 +12,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var can_double_jump : bool = false
 var double_jump_timer : float = 0.0
 var num_jumps : int = 0
+var can_control : bool = true
+
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
 
 func _physics_process(delta):
+	if not can_control: return
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -36,11 +43,37 @@ func _physics_process(delta):
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
+	#Flip the sprite to face the direction it is moving
+	if direction != 0:
+		sprite_2d.flip_h = direction < 0
+	
+	handle_animations(direction)
+	
 	move_and_slide()
 
+func handle_animations(direction : float) -> void:
+	if abs(direction) > 0.1 and is_on_floor():
+		animation_player.play("Running")
+	elif not is_on_floor():
+		animation_player.play("Jumping")
+	else:
+		animation_player.play("Idle")
+		
 func _process(delta):
 	if not is_on_floor():
 		double_jump_timer += delta
 		if double_jump_timer > coyote_time:
 			can_double_jump = true
+
+func handle_danger() -> void:
+	visible = false
+	can_control = false
+	
+	await get_tree().create_timer(1).timeout
+	reset_player()
+	
+func reset_player() -> void:
+	global_position = LevelManager.loaded_level.level_start_pos.global_position
+	visible = true
+	can_control = true
