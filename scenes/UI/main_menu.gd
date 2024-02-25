127519c88c1cbb8224f69a43fab6e486f8978222
JulianMartinez <jbpmartinez.cvt@eac.edu.ph>
2024-02-25 11:23:18 +0800

    Main menu update
    unfinished

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,6 +1,22 @@
 class_name MainMenu
-
 extends Control
+@onready var menu = $Background/MarginContainer/Menu
+@onready var settings = $Settings
+@onready var video = $Video
+@onready var audio = $Audio
+
+
+
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle()
+		
+		
+func toggle():
+	visible = !visible
+	get_tree().paused = visible
 
 
 
@@ -9,7 +25,12 @@ func _on_play_button_pressed():
 	deactivate()
 
 func _on_settings_button_pressed():
-	pass # Replace with function body.
+	show_and_hide(settings, menu)
+	
+
+func show_and_hide(first, second):
+	first.show()
+	second.hide()
 
 
 func _on_quit_button_pressed():
@@ -28,3 +49,28 @@ func activate() -> void:
 	set_process(true)
 	set_process_unhandled_input(true)
 	set_process_input(true)
+
+
+func _on_video_pressed():
+	show_and_hide(video, settings)
+
+
+func _on_audio_pressed():
+	show_and_hide(audio, settings)
+
+
+func _on_back_settings_pressed():
+	show_and_hide(settings, menu)
+
+
+func _on_fullscreen_toggled(toggled_on):
+	OS.WINDOW_FULLSCREEN
+
+
+
+func _on_borderless_toggled(toggled_on):
+	OS.window_borderless = button_pressed
+
+
+func _on_v_sync_toggled(toggled_on):
+	pass # Replace with function body.
