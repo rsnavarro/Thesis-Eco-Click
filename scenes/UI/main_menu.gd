d5d22f7ddeaf1d91006308920bdf7f5928a90f3a
JulianMartinez <jbpmartinez.cvt@eac.edu.ph>
2024-02-25 12:00:59 +0800

    Main menu

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -8,18 +8,6 @@ extends Control
 
 
 
-
-func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		toggle()
-		
-		
-func toggle():
-	visible = !visible
-	get_tree().paused = visible
-
-
-
 func _on_play_button_pressed():
 	LevelManager.load_level(1)
 	deactivate()
