a9a10f8b207e808db7a610ff1f9ef7934ef4a421
JulianMartinez <jbpmartinez.cvt@eac.edu.ph>
2024-03-01 00:10:01 +0800

    updated timer
    Fixed bug where timer exceeds timer limit of 0:00

--- scenes/UI/Timer.gd
+++ scenes/UI/Timer.gd
@@ -1,29 +1,31 @@
 extends Control
 
+var seconds = 0
+var minutes = 0
+var Dseconds = 0
+var Dminutes = 5
 
 func _ready():
 	Reset_Timer()
 	pass # Replace with function body.
 
-
-var seconds = 0
-var minutes = 0
-var Dseconds = 30
-var Dminutes = 1
-
-
-
 func _on_timer_timeout():
-	if seconds ==0:
-		if minutes>0:
-			minutes-=1
-			seconds=60
-	seconds-=1
-	
-	$Label.text=str(minutes)+":"+str(seconds)
+	if seconds == 0:
+		if minutes > 0:
+			minutes -= 1
+			seconds = 60
+
+	seconds -= 1
+
+	if minutes < 0: # Prevent negative minutes
+		minutes = 0
+
+	if seconds < 0: # Prevent negative seconds
+		seconds = 0
+
+	$Label.text = str(minutes) + ":" + str(seconds)
 	pass # Replace with function body.
 
-
 func Reset_Timer():
-	seconds=Dseconds
-	minutes=Dminutes
+	seconds = Dseconds
+	minutes = Dminutes
